3bdb9a98332f85e96d83a8176af6686549bdd7c9
Tsiky <andrianirinasoat@gmail.com>
2022-10-06 23:05:15 +0300

    #

--- components/Digitalisation/Add article/src/Div_left.tsx
+++ components/Digitalisation/Add article/src/Div_left.tsx
@@ -3,8 +3,8 @@ import style from "./add_articla.module.css";
 function Div_left(){
     return(
         <div className={style.image}>
-                <img className="im_background" src="./Image/pexels-tnarg-2685254.jpg" alt=""/>
-            </div>
+                <img className={style.im_background} src="./Image/pexels-tnarg-2685254.jpg" alt=""/>
+        </div>
     )
 }
 

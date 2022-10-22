7e107d8fddcd5a6a42d51d666baa81dd4f1715f2
Pana <panarandri@gmail.com>
2022-10-22 20:56:27 +0300

    Update Accueil.tsx

--- components/home/Accueil.tsx
+++ components/home/Accueil.tsx
@@ -1,7 +1,6 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import accueil from './accueil.module.css';
-import Register from "../Inscripotion_page/Register";
 import Category from "./Category";
 import BestSales from "./BestSales";
 import Sary from "./Sary";

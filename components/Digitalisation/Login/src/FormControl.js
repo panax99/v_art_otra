0dfe2efdb0648fde588239efa8e84ab083758cd4
Pana <panarandri@gmail.com>
2022-10-22 20:32:06 +0300

    Delete components/Digitalisation directory

--- components/Digitalisation/Login/src/FormControl.js
+++
@@ -1,9 +0,0 @@
-function FormControl(props){
-    return <>
-    <form className="user">
-            <input className="input_email input_styled" type="email" id="IdEmail" aria-describedby="emailHelp" placeholder="Email" name="email" />
-            <input className="input_password input_styled" type="password" id="Password" placeholder="Password" name="password" />
-          </form>
-    </>}
-
-export default FormControl;

5b536dcfe9d9506ba76c6ac5c8fae095a253ec19
Pana <panarandri@gmail.com>
2022-10-22 20:50:24 +0300

    Update Sary.tsx

--- components/home/Sary.tsx
+++ components/home/Sary.tsx
@@ -1,9 +1,10 @@
 import accueil from "./accueil.module.css"
+import Image from "next/image"
 export default function Sary(){
     return(
         <>
             <div className={accueil.fill}>
-            <img src="fond.png" className="i" alt=""/>
+            <Image src="fond.png" className="i" alt="papa"/>
             <div className="container" 
             style={
                 {
@@ -18,7 +19,7 @@ export default function Sary(){
             }>
                 <div style={{marginLeft:'20px',marginTop:'58px'}}>
                     <h3 style={{opacity:1}}>The best marketplace <br/>for all your art<br/> research</h3>
-                    <button className="btn btn-dark" style={{marginTop:'30px',width:'186px'}}><h5>Let's begin</h5></button>
+                    <button className="btn btn-dark" style={{marginTop:'30px',width:'186px'}}><h5>Let&apos;s begin</h5></button>
                 </div>
             </div>                      
 

65823b1eedd1087999bac4943a8a10d844887244
Pana <panarandri@gmail.com>
2022-10-22 20:31:11 +0300

    Update Header.tsx

--- components/home/Header.tsx
+++ components/home/Header.tsx
@@ -1,47 +1,45 @@
-
 import header from './header.module.css'
-
+import Link from 'next/link'
 function Header(){
     return(
         <>
             <div className={"navbar navbar-dark bg-dark "+header.a}>
                 <div className="container ml-50 mt-30">
-                    <a href="#">
-                        <h2 className={"text-light "+header.title} style={{top:"sticky"}}>V'Art'otra</h2>
-                    </a>
+                    <Link href="#">
+                        <h2 className={"text-light "+header.title} style={{top:"sticky"}}>V&apos;Art&apos;otra</h2>
+                    </Link>
                     <div className="form-group">
                         <input className="form-control form-control-color-dark" id={header.search} type="text" placeholder="Search"/>
                     </div>
                 </div>
                 <div className={header.l1}>
-                    <a href="/" className={header.link} style={{ color:"white", fontSize:'larger', fontWeight:'bolder', textDecoration:'none' ,fontStyle:"ubuntu  "}}>Home</a>
+                    <Link href="/" className={header.link} style={{ color:"white", fontSize:'larger', fontWeight:'bolder', textDecoration:'none' ,fontStyle:"ubuntu  "}}>Home</Link>
                 </div>
                 <div style={{width:'55px',height:'2px',background:'white',position:'absolute',left:'150px',top:'134px'}}></div>
                 <div className={header.l2}>
-                    <a href="/" className={header.link} style={{ color:"white", fontSize:'larger', fontWeight:'bolder', textDecoration:'none' }}>Paint</a>
+                    <Link href="/" className={header.link} style={{ color:"white", fontSize:'larger', fontWeight:'bolder', textDecoration:'none' }}>Paint</Link>
                 </div>
                 <div className={"container "+header.l3}>
-                    <a href="/" className={header.link} style={{ color:"white", fontSize:'larger', fontWeight:'bolder', textDecoration:'none' }}>Crafting</a>
+                    <Link href="/" className={header.link} style={{ color:"white", fontSize:'larger', fontWeight:'bolder', textDecoration:'none' }}>Crafting</Link>
                 </div>
                 <div className={"container "+header.l4}>
-                    <a href="/" className={header.link} style={{ color:"white", fontSize:'larger', fontWeight:'bolder', textDecoration:'none' }}>Sculpture</a>
+                    <Link href="/" className={header.link} style={{ color:"white", fontSize:'larger', fontWeight:'bolder', textDecoration:'none' }}>Sculpture</Link>
                 </div>
                 <div className={"container "+header.l5 } 
                     style={{ color:"white"}}>
-                    <a href="/" className={header.link} style={{ color:"white", fontSize:'larger', fontWeight:'bolder', textDecoration:'none'}}>Book</a>
+                    <Link href="/" className={header.link} style={{ color:"white", fontSize:'larger', fontWeight:'bolder', textDecoration:'none'}}>Book</Link>
                 </div>
                 <div className={"container "+header.l6} >
-                    <a href="/" className={header.link} style={{ color:"white", fontSize:'larger', fontWeight:'bolder', textDecoration:'none' }}>Others</a>
+                    <Link href="/" className={header.link} style={{ color:"white", fontSize:'larger', fontWeight:'bolder', textDecoration:'none' }}>Others</Link>
                 </div>
                 <div className={"container "+header.l7}>
-                    <a href="/login" className={header.link} style={{ color:"white", fontSize:'larger', fontWeight:'bolder', textDecoration:'none' }}>Login</a>
+                    <Link href="/login" className={header.link} style={{ color:"white", fontSize:'larger', fontWeight:'bolder', textDecoration:'none' }}>Login</Link>
                 </div>
                 <div className={header.l8}>
-                    <a href="/register" className={header.link} style={{ color:"white", fontSize:'larger', fontWeight:'bolder', textDecoration:'none' }}>Sign-Up</a>
+                    <Link href="/register" className={header.link} style={{ color:"white", fontSize:'larger', fontWeight:'bolder', textDecoration:'none' }}>Sign-Up</Link>
                 </div>
            </div>
         </>
     )
 }
-
 export default Header

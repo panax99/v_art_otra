54fd80d5ff828793c01a641c71bcf271475d7591
Pana <panarandri@gmail.com>
2022-10-22 20:41:46 +0300

    Update Login.tsx

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -1,23 +1,24 @@
-import { NextPage } from "next";
 import Head from "next/head";
-/*import  '../components/Digitalisation/Login/public/assets/bootstrap/css/bootstrap.min.css'
-import  '../components/Digitalisation/Login/public/assets/fonts/fontawesome-all.min.css'*/
-import style from '../components/Digitalisation/Login/public/Style.module.css'
-import Login_page from "../components/Digitalisation/Login/src/Login_page";
+import FormControl from "./FormControl";
 
-const Login : NextPage = () =>{
+function Login(){
     return(
-        <>
-            <Head>
-                <title>V-art-otra</title>
-                <meta name="description" content="Arts marketplace for everyone" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <div className={style.body}>
-                <Login_page/>
-            </div>
-        </>
+    <>
+
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="card shadow-lg o-hidden border-0 my-5 background" style={{height:"600px"}}>
+          <h4 className="Entete">Sign in</h4>
+          <FormControl/>
+          <hr className="ligne_1" />
+          <button className="button_login button_styled" type="submit">Login</button>
+          <hr className="ligne_2" />
+          <button className="button_login_with_google button_styled" type="submit">Connect with Google</button>
+          <button className="button_login_with_facebook button_styled" type="submit">Connect with Facebook</button>
+        </div>
+      </div>
+    </div>
+    </>
     )
 }
-
-export default Login;
+export default Login

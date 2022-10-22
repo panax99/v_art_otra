a9830917edeb1a75082df506a33d4c3324ca63a0
Pana <panarandri@gmail.com>
2022-10-22 20:35:00 +0300

    Update Footer.tsx

--- components/home/Footer.tsx
+++ components/home/Footer.tsx
@@ -5,7 +5,7 @@ function Footer(){
     return(
         <>
             <div className={"navbar navbar-dark bg-dark "+footer.ft}>
-                <h3 className="text-light" style={{marginLeft:'20px',marginBottom:'70px'}}>V'Art'Otra</h3><br/>
+                <h3 className="text-light" style={{marginLeft:'20px',marginBottom:'70px'}}>V&apos;Art&apos;Otra</h3><br/>
                 <br/>
                 <h6 className="text-light" style={{position:'absolute',top:'52px',left:'22px'}}>The website for all<br/> of your art research.</h6>
                 <h6 className="text-light" style={{position:'absolute',top:'52px',left:'1210px'}}>Copyright 2022</h6>

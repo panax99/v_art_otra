1e0da8e1db464fc33596812d1a5a795b10188bac
Pana <panarandri@gmail.com>
2022-10-22 23:05:56 +0300

    Update Propose.tsx

--- components/home/Propose.tsx
+++ components/home/Propose.tsx
@@ -2,7 +2,7 @@
 function Propose(){
     return(
         <>
-        <div style={{background:'ghostwhite',marginTop:'35px',marginBottom:'45px'}}>
+        <div style={{background:'ghostwhite',marginTop:'35px',marginBottom:'60px'}}>
             <div className="container" style={{
                 width:'650px',
                 height:'380px',
@@ -20,7 +20,7 @@ function Propose(){
                 </p>
                 <p style={{fontSize:'20px'}}>Our first initiative is to promote Malagasy Art in every aspects<br/> of his beauty. That is a platform that help the customers to find <br/>what they need and the artists to expose their art.</p>
                 <input type="text" className="form-control form-control-dark" style={{width:'200px'}} name="" id="" />
-                <button className="btn btn-dark" style={{marginLeft:'215px',marginTop:'-53px'}}>Send Feedback</button>
+                <button className="btn btn-dark" style={{marginLeft:'215px',marginTop:'-57px'}}>Send Feedback</button>
             </div>
         </div>
         </>

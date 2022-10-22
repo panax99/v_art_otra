9d36c9714b053445dc781d680d59a2719667c41e
Pana <panarandri@gmail.com>
2022-10-22 22:32:58 +0300

    Update Propose.tsx

--- components/home/Propose.tsx
+++ components/home/Propose.tsx
@@ -15,10 +15,10 @@ function Propose(){
 
             </div>
             <div className="jumbotron">
-                <p style={{fontSize:'45px',position:'absolute',top:'800px',left:'700px'}}>
+                <p style={{fontSize:'45px',position:'absolute',top:'300px',left:'700px'}}>
                     What this website<br/>propose to you?
                 </p>
-                <p style={{position:'absolute',top:'1800px',left:'700px',fontSize:'20px'}}>Our first initiative is to promote Malagasy Art in every aspects<br/> of his beauty. That is a platform that help the customers to find <br/>what they need and the artists to expose their art.</p>
+                <p style={{position:'absolute',top:'320px',left:'700px',fontSize:'20px'}}>Our first initiative is to promote Malagasy Art in every aspects<br/> of his beauty. That is a platform that help the customers to find <br/>what they need and the artists to expose their art.</p>
                 <input type="text" className="form-control form-control-dark" style={{position:'absolute',top:'1920px',left:'500px',width:'200px'}} name="" id="" />
                 <button className="btn btn-dark" style={{position:'absolute',top:'1920px',left:'1010px'}}>Send Feedback</button>
             </div>

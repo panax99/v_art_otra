1ca3286eb802c4f2145e1ad8a26ef5ac340e7aa5
Pana <panarandri@gmail.com>
2022-10-22 20:33:50 +0300

    Update Category.tsx

--- components/home/Category.tsx
+++ components/home/Category.tsx
@@ -1,5 +1,5 @@
 import accueil from "./accueil.module.css"
-
+import Image from "next/image"
 function Category(){
     return(
     <>
@@ -8,21 +8,21 @@ function Category(){
                 <h1 className={accueil.tt}>Category</h1>
         </div>
         <div className="fill">
-            <a href="#"><img src="paint.gif" className={"jumbotron "+accueil.ct1}/></a>            
+            <a href="#"><Image src="paint.gif" className={"jumbotron "+accueil.ct1}/></a>            
         </div>
         <div >
             <a href="#">
-            <img src="/craft.gif" alt="" style={{}} className={"jumbotron "+accueil.ct2}/>
+            <Image src="/craft.gif" alt="" style={{}} className={"jumbotron "+accueil.ct2}/>
             </a>
         </div>
         <div>
             <a href="#">
-            <img src="/sculpture.gif" alt="" style={{}} className={"jumbotron "+accueil.ct3}/>
+            <Image src="/sculpture.gif" alt="" style={{}} className={"jumbotron "+accueil.ct3}/>
             </a>
         </div>
         <div >
             <a href="#" style={{}}>
-            <img src="/books.gif" alt="" style={{}} className={"jumbotron "+accueil.ct4}/>
+            <Image src="/books.gif" alt="" style={{}} className={"jumbotron "+accueil.ct4}/>
             </a>        
         </div>
     </div>

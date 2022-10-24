aed2dc6d5ee33804d668c75431b47d97bca2e5f6
Pana <panarandri@gmail.com>
2022-10-24 17:29:23 +0300

    Update Header.tsx

--- components/home/Header.tsx
+++ components/home/Header.tsx
@@ -13,7 +13,7 @@ function Header(){
                     </div>
                 </div>
                 <div className={header.l1}>
-                    <Link href="#"><a style={{ color:"white", fontSize:'larger', fontWeight:'bolder', textDecoration:'none'}}>Home</a></Link>
+                    <Link href="#"><p style={{ color:"white", fontSize:'larger', fontWeight:'bolder', textDecoration:'none'}}>Home</p></Link>
                 </div>
                 <div style={{width:'55px',height:'2px',background:'white',position:'absolute',left:'150px',top:'134px'}}></div>
                 <div className={header.l2}>
